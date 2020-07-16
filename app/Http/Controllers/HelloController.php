7a0d07a68b5a2b32afc97c71ddec33335e633c8c
urayusuke <crfnf690@yahoo.co.jp>
2020-07-16 21:02:43 +0900

    provider add

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,11 +8,6 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
     public function index() {
-        $data = [
-            ['name'=>'山田太郎','mail'=>'taro@yamada'],
-            ['name'=>'田中花子','mail'=>'hanako@flower'],
-            ['name'=>'鈴木幸子','mail'=>'sachico@happy']
-    ];
-        return view('hello.index', ['data'=>$data]);
+    return view('hello.index', ['message'=>'Hello!']);
     }
 }
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,9 +9,9 @@
 
 @section('content')
   <p>ここが本文のコンテンツです。</p>
-  <ul>
-    @each('components.item',$data,'item')
-  </ul>
+  <p>Conteroller value<br>'message' = {{$message}}</p>
+  <p>ViewComposer value<br>'view_message' = {{$view_message}}
+  </p>
 @endsection
 
 @section('footer')
