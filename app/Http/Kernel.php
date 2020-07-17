bc5491790e42534ab35f2fb1ad964c27221cbd36
urayusuke <crfnf690@yahoo.co.jp>
2020-07-17 21:54:10 +0900

    index update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\HelloMiddleware::class,
     ];
 
     /**
@@ -43,6 +44,9 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+        'helo' => [
+            \App\Http\Middleware\HelloMiddleware::class,
+        ],
     ];
 
     /**
@@ -63,5 +67,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'hello' => \App\Http\Middleware\HelloMiddleware::class,
     ];
 }
