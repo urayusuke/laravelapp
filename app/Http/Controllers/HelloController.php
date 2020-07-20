ad4d51f71a45a73634ff9d8c71dec939b0cdb47f
urayusuke <crfnf690@yahoo.co.jp>
2020-07-20 21:01:13 +0900

    rule uodate

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,20 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
     public function index(Request $request) {
-    return view('hello.index',['msg'=>'フォームを入力:']);
+        return view('hello.index',['msg'=>'フォームを入力下さい。']);
     }
 
-    public function post(Request $request) {
-        $validate_rule = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric|between:0,150',
-        ];
-        $this->validate($request, $validate_rule);
+    public function post(HelloRequest $request) {
         return view('hello.index', ['msg'=>'正しく入力されました！']);
-    }
+    }   
 }
