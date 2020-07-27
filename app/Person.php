76496d7caa7260a60a1dee601cfc0c2c462ca6a8
urayusuke <crfnf690@yahoo.co.jp>
2020-07-27 21:24:51 +0900

    update

--- app/Person.php
+++ app/Person.php
@@ -8,29 +8,42 @@ use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    );
+
     public function getData()
     {
         return $this->id . ': ' . $this->name . '(' . $this->age . ')';
     }
 
-    public function scopeNameEqual($query,$str)
+    public function boards()
     {
-        return $query->where('name',$str);
+        return $this->hasMany('App\Board');
     }
 
-    public function scopeAgeGreaterThan($query, $n)
-    {
-        return $query->where('age', '>=', $n);
-    }
+    // public function scopeNameEqual($query,$str)
+    // {
+    //     return $query->where('name',$str);
+    // }
 
-    public function scopeAgeLessThan($query, $n)
-    {
-        return $query->where('age', '<=', $n);
-    }
+    // public function scopeAgeGreaterThan($query, $n)
+    // {
+    //     return $query->where('age', '>=', $n);
+    // }
 
-    protected static function boot()
-    {
-        parent::boot();
-        static::addGlobalScope(new ScopePerson);
-    }
+    // public function scopeAgeLessThan($query, $n)
+    // {
+    //     return $query->where('age', '<=', $n);
+    // }
+
+    // protected static function boot()
+    // {
+    //     parent::boot();
+    //     static::addGlobalScope(new ScopePerson);
+    // }
 }
--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -9,10 +9,19 @@
 
 @section('content')
     <table>
-      <tr><th>Data</th></tr>
+      <tr><th>Person</th><th>Board</th></tr>
       @foreach($items as $item)
         <tr>
           <td>{{$item->getData()}}</td>
+          <td>
+            @if($item->boards != null)
+                <table width="100%">
+                @foreach($item->boards as $obj)
+                  <tr><td>{{$obj->getData()}}</td></tr>
+                @endforeach
+              </table>
+            @endif
+          </td>
         </tr>
       @endforeach
     </table>
